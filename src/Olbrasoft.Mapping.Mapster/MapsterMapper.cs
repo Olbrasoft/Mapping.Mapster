ad3bf5621685fe8c8dde805fadf4b48ba09fae1a
MatrixStore <matrixstore@email.cz>
2020-03-21 22:38:23 +0100

    ini project

---
+++ src/Olbrasoft.Mapping.Mapster/MapsterMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Olbrasoft.Mapping.Mapster
+{
+    public class MapsterMapper : IMapper
+    {
+        private readonly global::MapsterMapper.IMapper _mapper;
+
+        public MapsterMapper(global::MapsterMapper.IMapper mapper) => _mapper = mapper;
+
+        public TDestination MapTo<TDestination>(object source) => _mapper.Map<TDestination>(source);
+    }
+}
