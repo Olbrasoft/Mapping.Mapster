68315cd39b1501c49372be0c6f649e384a2eedcc
MatrixStore <matrixstore@email.cz>
2020-03-22 14:45:57 +0100

    refract

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Mapping.Mapster
-Impementation IMapper dependent on Mapster
+IMapper and IProjector implemented using a mapster
 
 ![Olbrasoft mapping Mapster Icon](https://raw.githubusercontent.com/Olbrasoft/Mapping.Mapster/master/src/Olbrasoft.Mapping.Mapster/olbrasoft-mapping-mapster.png)
---
+++ src/Olbrasoft.Mapping.Mapster/MapsterProjector.cs
@@ -0,0 +1,21 @@
+﻿
+using Mapster;
+using System.Linq;
+
+namespace Olbrasoft.Mapping.Mapster
+{
+//    public class MapsterProjector : IProjector
+//    {
+//        private readonly TypeAdapterConfig _config;
+
+//        public MapsterProjector(TypeAdapterConfig config)
+//        {
+//            _config = config;
+//        }
+
+//        public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source)
+//        {
+//            return source.ProjectToType<TDestination>(_config);
+//        }
+//    }
+}
