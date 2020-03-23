a3db7dadbd8dcbd6042d099e70181a463c4bb24d
MatrixStore <matrixstore@email.cz>
2020-03-23 18:36:14 +0100

    extend Mapper to ProjectTo

--- src/Olbrasoft.Mapping.Mapster/MapsterMapper.cs
+++ src/Olbrasoft.Mapping.Mapster/MapsterMapper.cs
@@ -1,4 +1,7 @@
-﻿namespace Olbrasoft.Mapping.Mapster
+﻿using Mapster;
+using System.Linq;
+
+namespace Olbrasoft.Mapping.Mapster
 {
     public class MapsterMapper : IMapper
     {
@@ -7,5 +10,7 @@
         public MapsterMapper(global::MapsterMapper.IMapper mapper) => _mapper = mapper;
 
         public TDestination MapTo<TDestination>(object source) => _mapper.Map<TDestination>(source);
+
+        public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source) => source.ProjectToType<TDestination>(_mapper.Config);
     }
 }
--- src/Olbrasoft.Mapping.Mapster/MapsterProjector.cs
+++ src/Olbrasoft.Mapping.Mapster/MapsterProjector.cs
@@ -1,21 +1,17 @@
-﻿
-using Mapster;
+﻿using Mapster;
 using System.Linq;
 
 namespace Olbrasoft.Mapping.Mapster
 {
-//    public class MapsterProjector : IProjector
-//    {
-//        private readonly TypeAdapterConfig _config;
+    public class MapsterProjector : IProjector
+    {
+        private readonly TypeAdapterConfig _config;
 
-//        public MapsterProjector(TypeAdapterConfig config)
-//        {
-//            _config = config;
-//        }
+        public MapsterProjector(TypeAdapterConfig config)
+        {
+            _config = config;
+        }
 
-//        public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source)
-//        {
-//            return source.ProjectToType<TDestination>(_config);
-//        }
-//    }
+        public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source) => source.ProjectToType<TDestination>(_config);
+    }
 }
